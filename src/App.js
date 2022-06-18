a8e0f982da4826bd3fa936b53cdd4bed54d140cc
Ebi <kpemiogokimiebi@gmail.com>
2022-06-18 18:30:56 +0200

    Initial commit. Clean up boilerplate.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import react from 'react';
+
+
+export const App = () =>  (
+    <div className="App">
+      <header className="App-header">
+       <p>Hello World</p>
+      </header>
+    </div>
+  );
+
+
