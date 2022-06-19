d081757fdb22b5e95b91be75a927e24d65ad9b29
Ebi <kpemiogokimiebi@gmail.com>
2022-06-19 22:48:48 +0200

    Add new component

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+const firebaseConfig = firebase.initializeApp({
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messageSenderID: "",
+  apiId: "",
+});
+
+export { firebaseConfig as firebase }; 
