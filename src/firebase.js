644b50abe3669f9835b076466bb2a12268ce69d2
Ebi <kpemiogokimiebi@gmail.com>
2022-06-20 19:25:49 +0200

    Add compnent and configuration

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 
 
@@ -5,13 +7,13 @@
 
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messageSenderID: "",
-  apiId: "",
+  apiKey: "AIzaSyBkswOectyT95YzzmojEjMxpHQfGmW62Gk",
+  authDomain: "task-manager-78024.firebaseapp.com",
+  projectId: "task-manager-78024",
+  storageBucket: "task-manager-78024.appspot.com",
+  messagingSenderId: "535310788591",
+  appId: "1:535310788591:web:c39bd2349b6f24bf95381a",
+  measurementId: "G-Y8T8819JWH",
 });
 
 export { firebaseConfig as firebase }; 
