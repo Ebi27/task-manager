828823568e808fa41e06fd71c09de907ae9fdff9
Ebi <kpemiogokimiebi@gmail.com>
2022-06-21 22:40:32 +0200

    Add new component for collated task data.

---
+++ src/constants/index.js
@@ -0,0 +1,6 @@
+export const collatedTasks = [
+  { Key: "INBOX", name: "Inbox" },
+  { Key: "TODAY", name: "Today" },
+  { Key: "NEXT_7_DAYS", name: "Next 7 days" },
+  { Key: "NEXT_MONTH", name: "Next Month" },
+];
