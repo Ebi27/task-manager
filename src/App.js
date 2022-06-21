edb3099061bd060d1355c7141519fdd0ee895fd5
Ebi <kpemiogokimiebi@gmail.com>
2022-06-21 22:41:12 +0200

    Minor changes to component  import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Header } from "./components/layout/Header";
-import { Content } from "./components/layout/content";
+import { Content } from "./components/layout/Content";
 
 
 
