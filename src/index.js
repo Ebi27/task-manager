f850cd78434052c0aeaaaff0846eec85340c3953
Ebi <kpemiogokimiebi@gmail.com>
2022-06-25 23:28:21 +0200

    Import new component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { render } from "react-dom";
-import "./index.css";
 import { App } from "./App";
+import "./App.scss";
 
 
 
