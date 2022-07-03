6536ff56332844ec5e30110eaba91cc1816fba1c
Ebi <kpemiogokimiebi@gmail.com>
2022-07-03 22:27:57 +0200

    Create component to access selected projects.

---
+++ src/context/selected-project-context.js
@@ -0,0 +1,18 @@
+import React, { createContext,useContext, useState } from "react";
+
+
+
+//context provides where to pass data down react compononents without having to use props. We have a provider and a consumer
+
+export const SelectedProjectContext = createContext(); //
+export const SelectedProjectProvider = ({ children }) => {    //children is destructured and rendered within the provider  
+  const { SelectedProject, setSelectedProject } = useState('INBOX');
+
+  return (     //access provider which gives the value and renders the children  and value can beaccessed from any level 
+    <SelectedProjectContext.Provider value={{ SelectedProject, setSelectedProject }}>   
+      {children}
+    </SelectedProjectContext.Provider>
+  );
+};
+
+export const useSelectedProjectValue = () => useContext(SelectedProjectContext); // gives us access to context which gives access to value at any level when called 
