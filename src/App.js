d3aef2fc76b131e0781575c74183b4187698c476
Ebi <kpemiogokimiebi@gmail.com>
2022-07-04 23:31:38 +0200

    Sidebar import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Header } from "./components/layout/Header";
 import { Content } from "./components/layout/Content";
+import { Sidebar } from "./components/layout/Sidebar";
 import { ProjectsProvider, SelectedProjectProvider } from './context';
 
 
