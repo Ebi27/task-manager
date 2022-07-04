ee957058b8fac2f979814ca5138382a29ba2dbf3
Ebi <kpemiogokimiebi@gmail.com>
2022-07-04 23:33:07 +0200

    refactor: context for passing state

--- src/context/selected-project-context.js
+++ src/context/selected-project-context.js
@@ -1,18 +1,24 @@
 import React, { createContext,useContext, useState } from "react";
-
+import PropTypes from 'prop-types';
 
 
 //context provides where to pass data down react compononents without having to use props. We have a provider and a consumer
 
 export const SelectedProjectContext = createContext(); //
 export const SelectedProjectProvider = ({ children }) => {    //children is destructured and rendered within the provider  
-  const { SelectedProject, setSelectedProject } = useState('INBOX');
+  const [SelectedProject, setSelectedProject] = useState('INBOX');
 
   return (     //access provider which gives the value and renders the children  and value can beaccessed from any level 
-    <SelectedProjectContext.Provider value={{ SelectedProject, setSelectedProject }}>   
+    <SelectedProjectContext.Provider 
+    value={{ SelectedProject, setSelectedProject }}
+    >   
       {children}
     </SelectedProjectContext.Provider>
   );
 };
 
 export const useSelectedProjectValue = () => useContext(SelectedProjectContext); // gives us access to context which gives access to value at any level when called 
+
+SelectedProjectProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
