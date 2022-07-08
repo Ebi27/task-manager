e95aeab571013ec38455e49bb184ba76c25a6282
Ebi <kpemiogokimiebi@gmail.com>
2022-07-08 17:22:50 +0200

    Feat-show projects, select and see active projects.

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,49 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FaChevronDown,
   FaInbox,
   FaRegCalendarAlt,
   FaRegCalendar,
 } from "react-icons/fa";
+import { useSelectedProjectValue } from '../../context';
+import { Projects } from '../Projects'
 
-
-
-export const Sidebar = () => (
-  <div className="sidebar" data-testid="sidebar">
-    <ul className="sidebar_generic">
-      <li>
+export const Sidebar = () => { 
+  const { setSelectedProject } = useSelectedProjectValue;
+  const [active, setActive] = useState('inbox')
+  const [showProjects, setShowProjects] = useState(true); //To have/display  a drop down 
+  console.log(Sidebar)
+; 
+  return (
+    <div className="sidebar" data-testid="sidebar">
+      <ul className="sidebar_generic">
+        <li data-testid="inbox" className="inbox">
+          <span>
+            <FaInbox />
+          </span>
+          <span>Inbox</span>
+        </li>
+        <li data-testid="today" className="today">
+          <span>
+            <FaRegCalendar />
+          </span>
+          <span>Today</span>
+        </li>
+        <li data-testid="next_7" className="next_7">
+          <span>
+            <FaRegCalendar />
+          </span>
+          <span>Next 7 days</span>
+        </li>
+        <li data-testid="next_month" className="next_month">
+          <span>
+            <FaRegCalendar />
+          </span>
+          Next Month
+        </li>
+      </ul>
+      <div className="sidebar_middle">
         <span>
-          <FaInbox />
+          <FaChevronDown />
         </span>
-        Inbox
-      </li>
-      <li>
-        <span>
-          <FaRegCalendar />
-        </span>
-        Today
-      </li>
-      <li>
-        <span>
-          <FaRegCalendar />
-        </span>
-        Next 7 days
-      </li>
-      <li>
-        <span>
-          <FaRegCalendar />
-        </span>
-        Next Month
-      </li>
-    </ul>
-    <div className="sidebar_middle">
-      <span>
-        {" "}
-        <FaChevronDown />
-      </span>
-      <h2>Projects</h2>
+        <h2>Projects</h2>
+      </div>
+      <ul className="sidebar_projects"> {showProjects && <Projects />} </ul>
+      {showProjects && <Projects />}
     </div>
-    <ul className="sidebar_projects"> Projects will be here!</ul>
-    Add project Component here
-  </div>
-);
+  );};
